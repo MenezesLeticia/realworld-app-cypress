ca76ac49ff90886d7e44ecfe7f6fb72d681ca264
Leticia Menezes <dev.leticiamenezes@gmail.com>
2025-04-04 18:04:38 -0300

    Add selectors

--- cypress/tests/login.spec.ts
+++ cypress/tests/login.spec.ts
@@ -1,21 +1,27 @@
 describe('Login', () => {
-  it('Sucessfuly', () => {
+
+  const selectorsList= {
+    usernameField: "#username",
+    passwordField: "#password",
+    loginButtom: '[data-test="signin-submit"]',
+    logoutButtom: '[data-test="sidenav-signout"]',
+    wrongAlert: '[data-test="signin-error"]',
+  }
+
+  it('Sucessfuly', () => {  
     cy.visit('/signin')
-    cy.get('#username').type("Dina20")
-    cy.get('#password').type("s3cret")
-    cy.get('[data-test="signin-submit"]').click()
-    cy.get('[data-test="sidenav-signout"]').should("be.visible")
-
+    cy.get(selectorsList.usernameField).type("Dina20")
+    cy.get(selectorsList.passwordField).type("s3cret")
+    cy.get(selectorsList.loginButtom).click()
+    cy.get(selectorsList.logoutButtom).should("be.visible")
   });
-});
 
-describe('Login', () => {
   it('Invalid credentials', () => {
     cy.visit('/signin')
-    cy.get('#username').type("teste")
-    cy.get('#password').type("teste")
+    cy.get(selectorsList.usernameField).type("teste")
+    cy.get(selectorsList.passwordField).type("teste")
     cy.get('[data-test="signin-submit"]').click()
-    cy.get('[data-test="signin-error"]').should("be.visible")
-    
+    cy.get('[data-test="signin-error"]').should("be.visible")    
   });
 });
+
